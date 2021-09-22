a08c7333fd81ae39328d2e36f20b733ced238cf1
Fara Akhatova <faraakhatova@gmail.com>
2021-09-22 12:31:39 -0600

    Add jsx elements for popup component and invoke in App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@
 import Random from '../Random/Random'
 import ErrorPage from '../ErrorPage/ErrorPage'
 import { ApiData } from '../API/AppiCalls';
+import PopUp from '../Modal/PopUp';
 import './App.css';
 // ApiData.fetchAll() fetches object
 
@@ -12,11 +13,8 @@ function App() {
       <header>
       <h1 className="title"> RAG Time </h1>
       </header>
-      {/* <article className="intro-container">
-      <p> here is gonna be intro and rules about application</p>
-      </article>
-      <button className="start-btn"> Lets get started </button> */}
       {/* <Random/> */}
+      <PopUp trigger={true}/>
     </main>
     </div>
   );
