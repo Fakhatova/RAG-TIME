98395e7b29575cfc6e544aff430984131f776a4b
Fara Akhatova <faraakhatova@gmail.com>
2021-09-22 11:50:57 -0600

    add error handle for fetch requests status

--- src/Components/API/AppiCalls.js
+++ src/Components/API/AppiCalls.js
@@ -8,3 +8,11 @@
       .then(res => res.json())
  }
 }
+
+const checkForErrors = (res) => {
+  if(res.ok) {
+      return res.json()
+  }else {
+      throw `${res.status} ERROR. could not access server data.`
+  }
+}
