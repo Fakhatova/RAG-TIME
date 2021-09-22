0802102e0791f6f9c67a979a79d50b6471c69c35
Fara Akhatova <faraakhatova@gmail.com>
2021-09-22 10:12:31 -0600

    Add text element and bbutton for Random component

--- src/Components/App/Random/Random.js
+++ src/Components/App/Random/Random.js
@@ -9,6 +9,24 @@ const Random = () => {
 
          </select>
      </div>
+     <article className="activity-container">
+       <p>
+           Place for activity randomized
+       </p>
+       <p>
+           Place for activity catgory
+       </p>
+       <p>
+           Place for activity participants
+       </p>
+       <p>
+           Place for activity price
+       </p>
+       <p>
+           Place for activity assecibility
+       </p>
+     </article>
+     <button className="generate-btn"> Generate Activity</button>
     </section>
     )
 }
