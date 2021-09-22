efe234d6677b1ec7aeb8fa2409d65bb38ec6975f
Fara Akhatova <faraakhatova@gmail.com>
2021-09-22 14:26:29 -0600

    Add styling for background image

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,30 @@
 // import logo from './logo.svg';
-import {useState} from 'react'
-import Random from '../Random/Random'
-import ErrorPage from '../ErrorPage/ErrorPage'
-import { ApiData } from '../API/AppiCalls';
+import {useState, useEffect} from 'react'
+// import Random from '../Random/Random'
+// import ErrorPage from '../ErrorPage/ErrorPage'
+// import { ApiData } from '../API/AppiCalls';
 import PopUp from '../Modal/PopUp';
 import './App.css';
-import react from 'react'
 // ApiData.fetchAll() fetches object
 
 const  App = () => {
-  const [popup, setPopUp] = useState(false)
+  const [timedPopUp, setTimedPopUp] = useState(false)
+  
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //   setTimedPopUp(true)
+  //   }, 2500)
+  // })
 
   return (
     <div className="App-container">
     <main className="App">
       <header>
-      <h1 className="title"> RAG Time </h1>
+      {/* <h1 className="title"> RAG Time </h1> */}
       </header>
       {/* <Random/> */}
-      <PopUp trigger={true}/>
+      <PopUp trigger={timedPopUp}/>
     </main>
     </div>
   );
