a7a2f36ad00a9bb87999c21d1484a6fb76f00826
Fara Akhatova <faraakhatova@gmail.com>
2021-09-23 10:13:57 -0600

    Add select option for dropdown menu and add styling for select container

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -4,8 +4,19 @@ const Activity = () => {
     return (
     <section className="randomizer-container">
     <div className="dropdown-container">
-        <select className="selector">
-        </select>
+    <select name="drop-down" id="dropDown">
+    <option value="">--Please choose an option--</option>
+    <option value="all">All</option>
+    <option value="education">Education</option>
+    <option value="recreational">Recreational</option>
+    <option value="social">Social</option>
+    <option value="diy">DIY</option>
+    <option value="busywork">Busywork</option>
+    <option value="music">Music</option>
+    <option value="charity">Charity</option>
+    <option value="cooking">Cooking</option>
+    <option value="relaxation">Relaxation</option>
+</select>
     </div>
     <article className="activity-container">
     <p>
