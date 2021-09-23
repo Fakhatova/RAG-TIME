3648c637f9154c41de1bb9b40724c0f3cfaf3b8b
Fara Akhatova <faraakhatova@gmail.com>
2021-09-23 11:20:30 -0600

    Add useState hooks and import API data method

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -1,10 +1,14 @@
+import { ApiData } from '../API/AppiCalls';
+import { useState } from 'react'
 import './Activity.css'
 
 const Activity = () => {
+    const [option, setOptionValue] = useState('');
+    const [activity, setActivity] = useState('');
     return (
     <section className="randomizer-container">
     <div className="dropdown-container">
-    <select name="drop-down" id="dropDown">
+    <select className="drop-down" id="dropDown">
     <option value="">--Please choose an option--</option>
     <option value="all">All</option>
     <option value="education">Education</option>
@@ -16,23 +20,11 @@ const Activity = () => {
     <option value="charity">Charity</option>
     <option value="cooking">Cooking</option>
     <option value="relaxation">Relaxation</option>
-</select>
+    </select>
     </div>
     <article className="activity-container">
     <p>
         Place for activity randomized
-    </p>
-    <p>
-        Place for activity catgory
-    </p>
-    <p>
-        Place for activity participants
-    </p>
-    <p>
-        Place for activity price
-    </p>
-    <p>
-        Place for activity assecibility
     </p>
     <div className="Btn-container">
     <button className="generate-btn"> Generate Activity</button>
