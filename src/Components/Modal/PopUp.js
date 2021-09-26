5c6868c075d83e381e4ad0af7b6d0719457a85b0
Fara Akhatova <faraakhatova@gmail.com>
2021-09-26 14:16:46 -0400

    add spacing for and font size for popup modal

--- src/Components/Modal/PopUp.js
+++ src/Components/Modal/PopUp.js
@@ -5,9 +5,16 @@ const PopUp = (props) => {
         <div className="popup"> 
         <article className="popup-inner">
         <p> Hi there! Are you bored out of your mind? I was created to help you with just that. Wait, what's that? You're not bored? Well, I am here to give you a random activity to do alone or with friends, regardless.
+            <br/>
+            <br/>
         Step 1: Choose a category or go full random. You have 8 categories from which to choose (choose wisely or don't).
+            <br/>
+            <br/>
         Step 2: Now the fun part. Click the 'Generate Activity' button and BOOM! A random activity at your fingertips. I'm awesome, aren't I? 
+            <br/>
+            <br/>
         Step 3: Now, the most important step: GO OUT AND ENJOY THE ACTIVITY! I am just a bunch of 1s and 0s, so I can't join you, but do have all the fun!
+            <br/>
         Oh almost forgot, you can favorite activity you most enjoyed! Woooah!
         </p>
         </article>
