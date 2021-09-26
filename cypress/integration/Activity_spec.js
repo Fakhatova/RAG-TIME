9149d4bd1c8ab2d0b91935dcccf447bbcb054be7
Fara Akhatova <faraakhatova@gmail.com>
2021-09-26 18:52:26 -0400

    Add testing for activity page be visible

--- cypress/integration/Activity_spec.js
+++ cypress/integration/Activity_spec.js
@@ -1,8 +1,13 @@
 beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    cy.get('.App')
-    .should('be.visible')
-    cy.get('.popup-inner')
-        .get('p').contains('Hi there!')
-        .get('button').click()
+    cy.get('button').click()
+        cy.url().should('eq', 'http://localhost:3000/Activity')
+        cy.visit('http://localhost:3000/Activity')
+})
+
+describe('Activity Page', () => {
+    it('Should be able see new page after clicking lets start button', () => {
+    cy.visit('http://localhost:3000/Activity')
+        cy.get('.Activity-container').should('be.visible')
+    })
 })
