2d62195fcb8dcf05fdc11539ce168c8694e9874a
Fara Akhatova <faraakhatova@gmail.com>
2021-09-26 18:19:01 -0400

    Add test for popup modal and text content and button

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -5,6 +5,13 @@ beforeEach(() => {
 describe('Home-Page', () => {
     it('Should be able to see home page', () => {
         cy.get('.App')
+        .should('be.visible')
+    })
+
+    it('Should be able to see pop up activity with instructions and click start', () => {
+        cy.get('.popup-inner')
+        .get('p').contains('Hi there!')
+        .get('button').click()
     })
 })
 
